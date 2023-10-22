71322138914b4564fc3617e9ce904daaa0456ea5
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-22 09:07:53 +0200

    finished

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,26 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <p>There are ${employees.length} employees.</p>
 <ul>
 	${employees.map(employee => {
-		return /*html*/ `
-			<li>${employee.firstName} <strong>${employee.lastName}</strong> - ${employee.address.city}</li>	
+	return /*html*/ `
+			<li>${employee.firstName} <strong>${employee.lastName}</strong> - ${employee.address.city}
+			<ul>
+				<li>reports to: <span class="reportsTo">${getManagerName(Number(employee.reportsTo))}</span></li>
+			</ul>
+			</li>
 		`
-	}).join('')}
+}).join('')}
 </ul>
 `;
+
+function getManagerName(reportsTo: number) {
+	if (isNaN(reportsTo)) {
+		return 'nobody';
+	} else {
+		const employee = employees.find(m => m.employeeID === reportsTo);
+		if (employee) {
+			return employee.firstName + ' ' + employee.lastName;
+		} else {
+			return `(error: employee #${reportsTo} does not exist)`;
+		}
+	}
+}
